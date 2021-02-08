2bf0bad43acecdf58ca039fe5fab224007413c41
parfenovichs <CoKoJl1k>
2021-02-08 15:37:59 +0300

    second commit third commit fourth commit fifth commit 6 commit 7 commit 10 commit 11 commit

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?
 require 'config/database.php';
 require 'config/paths.php';
-
-
-
-// new_branch
+// new_branch merge
 // Use an autoloader
 require 'libs/Bootstrap.php';
 require 'libs/Controller.php';
